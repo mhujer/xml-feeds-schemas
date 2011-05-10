3fed310130cb5a9bf37b07468cee72f45ac02dce
Martin Hujer <mhujer@gmail.com>
2011-05-10 19:39:26 +0200

    php validate

---
+++ php/validate.php
@@ -0,0 +1,18 @@
+<?php
+chdir('../');
+
+validate('jyxo.xml', 'jyxo.xsd');
+validate('jyxo-gen.xml', 'jyxo.xsd');
+validate('zbozi.xml', 'zbozi.xsd');
+validate('zbozi-gen.xml', 'zbozi.xsd');
+
+function validate ($xml, $schema)
+{
+    $doc = new DomDocument();
+    $doc->Load($xml);
+    if (@$doc->schemaValidate($schema)) {
+        printf('%s is valid against %s' . "\n", $xml, $schema);
+    } else {
+        printf('!!! %s is not valid against %s' . "\n", $xml, $schema);
+    }
+}
